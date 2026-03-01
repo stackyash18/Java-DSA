21269effdd3c2c94e8fdb4351b869b50f51ebd97
yash tomar <tomary880gmail.com>
2026-03-01 18:13:45 +0530

    Added the program files

---
+++ PaymentApp.java
@@ -0,0 +1,66 @@
+abstract class Payment {
+
+    protected String transactionId;
+
+    public Payment(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    //Implemented Abstract method  
+    abstract void pay(double amount);
+
+    // Common method shared by all payments
+    void generateReceipt(double amount) {
+        System.out.println("Transaction ID: " + transactionId);
+        System.out.println("Amount Paid: ₹" + amount);
+        System.out.println("Payment Successful");
+        System.out.println("--------------------------");
+    }
+}
+
+// UPI Payment
+class UPIPayment extends Payment {
+
+    private String upiId;
+
+    public UPIPayment(String transactionId, String upiId) {
+        super(transactionId);
+        this.upiId = upiId;
+    }
+
+    @Override
+    void pay(double amount) {
+        System.out.println("Processing UPI payment via ID: " + upiId);
+        generateReceipt(amount);
+    }
+}
+
+// Credit Card Payment
+class CreditCardPayment extends Payment {
+
+    private String cardNumber;
+
+    public CreditCardPayment(String transactionId, String cardNumber) {
+        super(transactionId);
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    void pay(double amount) {
+        System.out.println("Processing Credit Card payment using card: " + cardNumber);
+        generateReceipt(amount);
+    }
+}
+
+// Main class
+public class PaymentApp {
+
+    public static void main(String[] args) {
+
+        Payment upi = new UPIPayment("TXN1001", "yash@upi");
+        Payment card = new CreditCardPayment("TXN1002", "1234-5678-9012-3456");
+
+        upi.pay(1500);
+        card.pay(5000);
+    }
+}
