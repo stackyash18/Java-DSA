5119e92a63d63ab0f41f72d2e08b190f4d3822d3
yash tomar <tomary880@gmail.com>
2026-03-21 17:43:07 +0530

    Added the program files

--- yash1/AccessModifier.java
+++ yash1/AccessModifier.java
@@ -2,14 +2,14 @@ package yash1;
 
 
 public class AccessModifier {
-  void printYash()
-  {
-    System.out.println("Hello, Yash this side.");
-  } 
-  private void mogleyy()
-  {
-  System.out.println("Protected method.");
-  }
+   public int x = 5;
+   protected int y = 45;
+   int z = 6;
+   private b = 78;
+   public void printVar()
+   {
+   
+   }
   public static void main(String args[])
   {
 
