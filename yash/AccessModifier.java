de620d65887136bbb0b03d74c98caa50fe1d3794
yash tomar <tomary880@gmail.com>
2026-03-21 20:35:11 +0530

    Implemented the access modifier

---
+++ yash/AccessModifier.java
@@ -0,0 +1,27 @@
+package yash;
+class Working
+{
+  public int y=5;
+  protected int x=4;
+  int z=7;
+  private int c = 10;
+
+  public void meth()
+  {
+   System.out.println(x);
+   System.out.println(y);
+   System.out.println(z);
+   System.out.println(c);
+  }
+}
+public class AccessModifier {
+  public static void main(String args[])
+  {
+  Working w1 = new Working();
+  System.out.println(w1.y);
+  System.out.println(w1.z);
+ // System.out.println(w1.c); ~~ This Variable can't be accessed in same package because it has private access modifier. 
+  w1.meth();
+  }
+  
+}
