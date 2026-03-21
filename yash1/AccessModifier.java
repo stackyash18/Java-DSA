d6e6c8188253f191e906b46cdced1051cbdd651f
yash tomar <tomary880gmail.com>
2026-03-21 13:34:16 +0530

    Added program files

---
+++ yash1/AccessModifier.java
@@ -0,0 +1,18 @@
+package yash1;
+
+
+public class AccessModifier {
+  void printYash()
+  {
+    System.out.println("Hello, Yash this side.");
+  } 
+  private void mogleyy()
+  {
+  System.out.println("Protected method.");
+  }
+  public static void main(String args[])
+  {
+  AccessModier n = new AccessModifier();
+ 
+  }
+}
---
+++ yash1/uses.java
@@ -0,0 +1,10 @@
+package yash1;
+
+public class uses {
+  
+  public static void main(String args[])
+  {
+  AccessModifier y = new AccessModifier();
+  y.printYash();
+  }
+}
