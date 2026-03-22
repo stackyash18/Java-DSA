14c171ad0506db64f0030948773d2e5c17b9535e
yash tomar <tomary880@gmail.com>
2026-03-22 12:41:54 +0530

    Added the Calculator Class in Package

---
+++ calc/Calculator.java
@@ -0,0 +1,60 @@
+package calc;
+
+class SimpleCalculator
+{
+  public void addNum(int a, int b)
+  {
+  System.out.println("Your Result is: " + (a+b));
+  }
+
+  public void minusNum(int a, int b)
+  {
+  System.out.println("Your Result is: " + (a-b));
+  }
+  public void multNum(int a, int b)
+  {
+    System.out.println("Your Result is: " + (a*b));
+  }
+  public void divNum(int a, int b)
+  {
+  System.out.println("Your Result is: " + (a/b));
+  }
+}
+
+class ScCalculator
+{
+  public void sumOfSin(int a, int b)
+  {
+    System.out.println("The Value of sin(a+b) is:  " + Math.sin(a+b));
+  }
+
+   public void diffOfSin(int a, int b)
+  {
+    System.out.println("The Value of sin(a-b) is:  " + Math.sin(a-b));
+  }
+  
+   public void sumOfCos(int a, int b)
+  {
+    System.out.println("The Value of cos(a+b) is:  " + Math.cos(a+b));
+  }
+
+   public void diffOfCos(int a, int b)
+  {
+    System.out.println("The Value of cos(a-b) is:  " + Math.cos(a-b));
+  }
+}
+
+class HybCalc
+{
+  public void calcInterest(int noOfYears, float rate, int principalAmount)
+  {
+  float interest = (rate/100)*principalAmount;
+  float totalAmount = principalAmount + interest*noOfYears;
+  }
+}
+public class Calculator {
+  public static void main(String args[])
+  {
+    System.out.println("This is Calculator");
+  }
+}
