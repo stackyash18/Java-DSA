35665005dd22e7aada8c76175606ec3850ffd09f
yash tomar <tomary880@gmail.com>
2026-03-23 21:33:57 +0530

    Added the Programfile of Custom Class

---
+++ CustomPackage.java
@@ -0,0 +1,13 @@
+
+/* 
+You have to create a package named yash.in.shape
+This package should have individual classes for Rectangle, Square,Circle, Cylinder, Sphere
+These classes should use inheritance  to properly manage the code!
+Include method like volume,surfaceArea and getters/setters for dimensions 
+*/
+public class CustomPackage {
+  public static void main(String args[])
+  {
+
+  }
+}
