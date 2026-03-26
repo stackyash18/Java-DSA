daf370cfb0f8c69dc12e89a91fa61283259ecb27
yash tomar <tomary880@gmail.com>
2026-03-26 13:26:19 +0530

    Added the problem of Patterns

--- DSA/Patterns/A.java
+++ DSA/Patterns/A.java
@@ -1,5 +1,24 @@
 package DSA.Patterns;
+import java.util.Scanner;
 
+/*
+Pattern1.    * * * * *
+             * * * * *  
+             * * * * * 
+             * * * * * 
+                                     */
 public class A {
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    for(int i=0; i<4; i++)
+    {
+      for(int j=0; j<4; j++)
+      {
+        System.out.print("   *");
+      }
+      System.out.println();
+    }
+  }
   
 }
---
+++ DSA/Patterns/B.java
@@ -0,0 +1,8 @@
+package DSA.Patterns;
+
+public class B {
+  public static void main(String args[])
+  {
+    
+  }
+}
