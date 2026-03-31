564699b3b7884f1178c54fdd2296fa99edf5d47e
yash tomar <tomary880@gmail.com>
2026-03-31 10:18:09 +0530

    Added the Thread Constructor File

---
+++ Java/ThreadConstructor.java
@@ -0,0 +1,25 @@
+class MyRunnable implements Runnable
+{
+  String name;
+
+  MyRunnable(String name)
+  {
+    this.name = name;
+  }
+
+  public void run()
+  {
+    for(int i=1; i<=10; i++)
+    {
+      System.out.println("Thread Running: " + name);
+    }
+  }
+}
+public class ThreadConstructor {
+  public static void main(String args[])
+  {
+  MyRunnable obj = new MyRunnable("yash");
+   Thread t1 =  new Thread(obj);
+   t1.start();
+  }
+}
