7765cebbf306d2256fc2f987f4aa3ba65e32c5f0
yash tomar <tomary880@gmail.com>
2026-04-04 12:45:14 +0530

    Added the Thread Practice Questions Files

---
+++ Java/ThreadPractice.java
@@ -0,0 +1,40 @@
+class PracticeA extends Thread
+{
+ public void run()
+ {
+  while(true)
+  {
+    System.out.println("Good Morning.");
+  }
+ }
+}
+
+class PracticeB extends Thread
+{
+ public void run()
+ {
+  while(true)
+  { 
+    try
+    {
+      PracticeA.sleep(20);
+    }
+    catch (Exception e)
+    {
+      System.out.println(e);
+    }
+    System.out.println("Welcome.");
+  }
+ }
+}
+
+public class ThreadPractice {
+  public static void main(String args[])
+  {
+  PracticeA p1 = new PracticeA();
+  PracticeB p2 = new PracticeB();
+
+  p1.start();
+  p2.start();
+  }
+}
--- Java/ThreadPriorities.java
+++ Java/ThreadPriorities.java
@@ -24,6 +24,7 @@ public class ThreadPriorities
 
     t1.start();
     t2.start();
+    Thread.activeCount();
 
     
   }
