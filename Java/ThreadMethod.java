26901336264322dc927b1cfca5ede2951c97375f
yash tomar <tomary880@gmail.com>
2026-04-04 11:26:38 +0530

    Added the Thread method file

---
+++ Java/ThreadMethod.java
@@ -0,0 +1,30 @@
+class MyThread18 extends Thread
+{
+  public void run()
+  {
+    while(true)
+    {
+      System.out.println("Running...");
+      System.out.println("Thank you...");
+    }
+  }
+}
+public class ThreadMethod {
+  public static void main(String args[])
+  {
+  MyThread18 t1 = new MyThread18();
+  MyThread18 t2 = new MyThread18();
+  t1.start();
+  try
+  {
+    t1.join();
+  }
+  catch(Exception e)
+  {
+    System.out.println(e);
+  }
+
+  t2.start();
+
+  }
+}
