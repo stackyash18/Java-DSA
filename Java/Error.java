36dfc99671540c2fd7c21b1539e685e9dfc2b528
yash tomar <tomary880@gmail.com>
2026-04-15 03:07:49 +0530

    Adding the Error in java file

---
+++ Java/Error.java
@@ -0,0 +1,42 @@
+public class Error {
+  public static void main(String args[])
+  {
+  /*Syntax Error 
+   int a = 5 -->>
+   System.ott.println(); -->>
+   */
+
+   /*
+   Logical Error 
+   -->>to find the prime number 
+   System.out.println(2);
+   for(int i = 2; i <= 100; i++)
+   {
+   System.out.println(2*i+1);
+   }
+   
+   Correct Version
+   for(int num = 2; num <= 100; i++)
+   {
+   boolean isPrime = true;
+
+   for(int i = 2; i <= Math.sqrt; i++)
+   {
+   if(num % i == 0)
+   {
+   isPrime == 0;
+   break;
+   }
+   }
+
+   if(isPrime)
+   {
+   System.out.println(num +" ");
+   }
+   }
+   */
+  
+
+
+  }
+}
---
+++ Java/Sample.java
@@ -0,0 +1,24 @@
+public class Sample
+{
+  public static void main(String args[])
+  {
+    for(int num = 2; num <= 100; num++)
+    {
+      boolean isPrime = true;
+
+      for(int i = 2; i <= Math.sqrt(num); i++)
+      {
+        if(num % i == 0)
+        {
+          isPrime = false;
+          break;
+        }
+      }
+
+      if(isPrime)
+      {
+        System.out.println(num + " ");
+      }
+    }
+  }
+}
