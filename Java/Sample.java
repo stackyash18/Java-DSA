094002fd406e4e75610cf1466b9005b232034045
yash tomar <tomary880@gmail.com>
2026-07-07 21:45:11 +0530

    write the java code without  IDE

--- Java/Sample.java
+++ Java/Sample.java
@@ -3,6 +3,13 @@ public class Sample
 {
 public static void main(String args[])
 {
-System.out.println("Good morning");
+ for(int i=1; i<=5; i++)
+{
+for(int j=1; j<=i; j++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
 }
 }
