999880663fe0dc65df83767631803822e76b92f6
yash tomar <tomary880@gmail.com>
2026-07-08 23:41:39 +0530

    formatted the code

--- Java/AbstractClass.java
+++ Java/AbstractClass.java
@@ -1,54 +1,47 @@
-abstract class Pen
-{
+abstract class Pen {
   abstract void write();
+
   abstract void refill();
 }
-class FountainPen extends Pen
-{
-  void write()
-  {
-  System.out.println("Your can write with Fountain Pen.");
+
+class FountainPen extends Pen {
+  void write() {
+    System.out.println("Your can write with Fountain Pen.");
   }
-  void refill()
-  {
+
+  void refill() {
     System.out.println("Refill the Fountain Pen.");
   }
-  void changeNib()
-  {
+
+  void changeNib() {
     System.out.println("You have to change the nib of  Fountain Pen.");
   }
 }
 
-class CelloPen extends Pen
-{
-  void write()
-  {
-  System.out.println("Your can write with Cello Pen.");
+class CelloPen extends Pen {
+  void write() {
+    System.out.println("Your can write with Cello Pen.");
   }
-  void refill()
-  {
+
+  void refill() {
     System.out.println("Refill the Cello Pen.");
   }
-  void changeNib()
-  {
+
+  void changeNib() {
     System.out.println("You have to change the nib of  Cello Pen.");
   }
 }
 
-public class AbstractClass
-{
-  public static void main(String args[])
-  {
+public class AbstractClass {
+  public static void main(String args[]) {
 
-  FountainPen pen = new FountainPen();
-  pen.refill();
-  pen.changeNib();
-  
+    FountainPen pen = new FountainPen();
+    pen.refill();
+    pen.changeNib();
 
-  CelloPen pen2 = new CelloPen();
-  pen2.changeNib();
-  pen2.write();
-  
+    CelloPen pen2 = new CelloPen();
+    pen2.changeNib();
+    pen2.write();
 
   }
 }
--- Java/ToPrintTheElementOfArrayInReverseOrder.java
+++ Java/ToPrintTheElementOfArrayInReverseOrder.java
@@ -1,11 +1,10 @@
 public class ToPrintTheElementOfArrayInReverseOrder {
   public static void main(String[] args) {
-    int[] array = {10,20,30,40,50};
+    int[] array = { 10, 20, 30, 40, 50 };
 
     int n = array.length;
 
-    for(int i = n-1; i >= 0; i--)
-    {
+    for (int i = n - 1; i >= 0; i--) {
       System.out.print(array[i]);
     }
 
