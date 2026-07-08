d51b7bb6ec06c33168e5fa455dd973d3a9d5d782
yash tomar <tomary880@gmail.com>
2026-07-08 23:35:51 +0530

    formatted the code

--- Java/Error.java
+++ Java/Error.java
@@ -1,47 +1,46 @@
 public class Error {
-  public static void main(String args[])
-  {
-  /*Syntax Error 
-   int a = 5 -->>
-   System.ott.println(); -->>
-   */
+  public static void main(String args[]) {
+    /*
+     * Syntax Error
+     * int a = 5 -->>
+     * System.ott.println(); -->>
+     */
 
+    /*
+     * Logical Error
+     * -->>to find the prime number
+     * System.out.println(2);
+     * for(int i = 2; i <= 100; i++)
+     * {
+     * System.out.println(2*i+1);
+     * }
+     * 
+     * Correct Version
+     * for(int num = 2; num <= 100; i++)
+     * {
+     * boolean isPrime = true;
+     * 
+     * for(int i = 2; i <= Math.sqrt; i++)
+     * {
+     * if(num % i == 0)
+     * {
+     * isPrime == 0;
+     * break;
+     * }
+     * }
+     * 
+     * if(isPrime)
+     * {
+     * System.out.println(num +" ");
+     * }
+     * }
+     */
 
-   /*
-   Logical Error 
-   -->>to find the prime number 
-   System.out.println(2);
-   for(int i = 2; i <= 100; i++)
-   {
-   System.out.println(2*i+1);
-   }
-   
-   Correct Version
-   for(int num = 2; num <= 100; i++)
-   {
-   boolean isPrime = true;
+    /*
+     * Runtime Error
+     * int k = 0;
+     * System.out.println(1000/k);
+     */
 
-   for(int i = 2; i <= Math.sqrt; i++)
-   {
-   if(num % i == 0)
-   {
-   isPrime == 0;
-   break;
-   }
-   }
-
-   if(isPrime)
-   {
-   System.out.println(num +" ");
-   }
-   }
-   */
-  
-  /* 
-  Runtime Error
-  int k = 0;
-  System.out.println(1000/k);
-  */
- 
   }
 }
--- Java/GuessNumberUsingOOPs.java
+++ Java/GuessNumberUsingOOPs.java
@@ -1,77 +1,64 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class GuessNumber{
+class GuessNumber {
   private int num;
   private int noOfGuesses;
 
-
-  public GuessNumber(){
-    Random rand =  new Random();
-   num = rand.nextInt(100) + 1;
-   noOfGuesses = 0;
+  public GuessNumber() {
+    Random rand = new Random();
+    num = rand.nextInt(100) + 1;
+    noOfGuesses = 0;
   }
-  
- public int takeUserInput(Scanner sc)
- {
-  System.out.println("Enter you guesses: ");
-  return sc.nextInt();  
- }
 
-
- public boolean isCorrectNumber(int usernum)
- {
-  noOfGuesses ++;
-  
-  if(usernum > num)
-  {
-    System.out.println("Guess Lower");
-    return false;
-
+  public int takeUserInput(Scanner sc) {
+    System.out.println("Enter you guesses: ");
+    return sc.nextInt();
   }
-  else
-    if(usernum < num)
-    {
+
+  public boolean isCorrectNumber(int usernum) {
+    noOfGuesses++;
+
+    if (usernum > num) {
+      System.out.println("Guess Lower");
+      return false;
+
+    } else if (usernum < num) {
       System.out.println("Guess Higher");
       return false;
-    }
-    else
-   {
+    } else {
       System.out.println("Congratualations, You won");
       System.out.println("Total guesses: " + noOfGuesses);
-    return true;
+      return true;
     }
- }
-
   }
-public class GuessNumberUsingOOPs
-{
-  
-  public static void main(String[] args)
-  {
+
+}
+
+public class GuessNumberUsingOOPs {
+
+  public static void main(String[] args) {
     /*
-    Create a class Game, which allows a user to play "Guess the Number " game once. Game should have the following methods:
-    1.Contructors to generate the random number.
-    2.TakeUserInput to take a user input of number.
-    3.isCorrectNumber() to detect whether the number entered by the user is true.
-    4.Getters and Setters for noOfGuesses.
-     Use properties such as noOfGuesses(Int), etc to get  this task done!
-    */
+     * Create a class Game, which allows a user to play "Guess the Number " game
+     * once. Game should have the following methods:
+     * 1.Contructors to generate the random number.
+     * 2.TakeUserInput to take a user input of number.
+     * 3.isCorrectNumber() to detect whether the number entered by the user is true.
+     * 4.Getters and Setters for noOfGuesses.
+     * Use properties such as noOfGuesses(Int), etc to get this task done!
+     */
 
-     Scanner input = new Scanner(System.in);
-     GuessNumber g1 = new GuessNumber();
-     
-     boolean correct = false;
+    Scanner input = new Scanner(System.in);
+    GuessNumber g1 = new GuessNumber();
 
-     while(!correct)
-     {
+    boolean correct = false;
+
+    while (!correct) {
       int usernum = g1.takeUserInput(input);
       correct = g1.isCorrectNumber(usernum);
-     }
-     
-    
-    input.close();
+    }
 
+    input.close();
 
   }
 }
