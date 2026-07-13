6918b8407e1f0465b576b461d6d6b3b3bfcd9255
yash tomar <tomary880@gmail.com>
2026-07-13 21:34:18 +0530

    Formatted the code

--- Java/Array/MaxMinOfArray.java
+++ Java/Array/MaxMinOfArray.java
@@ -1,13 +1,13 @@
 package Array;
+
 import java.util.Scanner;
 
 public class MaxMinOfArray {
-  public static void main(String args[])
-  {
+  public static void main(String args[]) {
     Scanner input = new Scanner(System.in);
 
     System.out.println("<--Finding minimum and maximum element of an array-->");
-     
+
     int arr[] = ArrayUtility.inputArray();
 
     int maxElement = maxElement(arr);
@@ -18,26 +18,20 @@ public class MaxMinOfArray {
     input.close();
   }
 
-  public static int maxElement(int arr[])
-  {
+  public static int maxElement(int arr[]) {
     int maxElement = 0;
-    for(int i=0; i<arr.length; i++)
-    {
-      if(arr[i]>maxElement)
-      {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
         maxElement = arr[i];
       }
     }
     return maxElement;
   }
 
-  public static int minElement(int arr[])
-  {
+  public static int minElement(int arr[]) {
     int minElement = arr[0];
-    for(int i=1; i<arr.length; i++)
-    {
-      if(arr[i]<minElement)
-      {
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] < minElement) {
         minElement = arr[i];
       }
     }
--- Java/Array/MergeTwoSortedArray.java
+++ Java/Array/MergeTwoSortedArray.java
@@ -1,8 +1,7 @@
 package Array;
 
 public class MergeTwoSortedArray {
-  public static void main(String args[])
-  {
+  public static void main(String args[]) {
     System.out.println("Merging two sorted array.");
     System.out.println("Enter the sorted array1.");
     int arr1[] = ArrayUtility.inputArray();
@@ -13,39 +12,33 @@ public class MergeTwoSortedArray {
     System.out.println("The merged array is: ");
     ArrayUtility.displayArray(newArr);
   }
-  public static int[] mergeArray(int arr1[], int arr2[])
-  {
+
+  public static int[] mergeArray(int arr1[], int arr2[]) {
     int newArr[] = new int[arr1.length + arr2.length];
-    int i=0, j=0, k=0;
-    while(i<arr1.length && j<arr2.length)
-    {
-      if(arr1[i]<=arr2[j])
-      {
-       newArr[k] = arr1[i];
-       i++;
-      }
-      else
-      {
+    int i = 0, j = 0, k = 0;
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] <= arr2[j]) {
+        newArr[k] = arr1[i];
+        i++;
+      } else {
         newArr[k] = arr2[j];
         j++;
       }
       k++;
     }
 
-    while(i<arr1.length)
-    {
+    while (i < arr1.length) {
       newArr[k] = arr1[i];
       i++;
       k++;
     }
 
-    while (j<arr2.length)
-    {
+    while (j < arr2.length) {
       newArr[k] = arr2[j];
       j++;
       k++;
     }
-    
+
     return newArr;
   }
 }
