a09f026861cfacc34c3079f768a3b990835f0834
yash tomar <tomary880@gmail.com>
2026-07-14 23:04:41 +0530

    Created the Minimum From TwoNumber file

---
+++ Java/Practise Exercise/MinimumFromTwoNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MinimumFromTwoNumber {
+  public static void main(String args[])
+  {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the first number: ");
+    int a = input.nextInt();
+    System.out.println("Enter the second number: ");
+    int b = input.nextInt();
+
+    // in this question i have only two variable so ternary operator woubld be the best choice for simplicity.
+
+    int minNumber = a<b?a:b;
+    System.out.println(minNumber + " is the smaller number.");
+  }
+}
