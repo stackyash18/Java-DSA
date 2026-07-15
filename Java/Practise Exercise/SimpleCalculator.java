3ac1d923b6834f145db5fe72ae36e6417b66222b
yash tomar <tomary880@gmail.com>
2026-07-15 22:26:24 +0530

    Added the Simple calculator using switch statement

---
+++ Java/Practise Exercise/SimpleCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+  public static void main(String args[])
+  {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter you first number: ");
+    int num1 = input.nextInt();
+    System.out.print("Now enter your second number: ");
+    int num2 = input.nextInt();
+    System.out.print("Now enter the operation you want to perform: ");
+    String opr = input.next();
+
+    switch (opr) {
+      case opr:
+        
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+
+}
