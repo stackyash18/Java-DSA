10ac424247ff2e8b0991e61ededc9b59c7beb3b1
yash tomar <tomary880@gmail.com>
2026-07-17 20:35:55 +0530

    G

---
+++ Java/MathFunction.java
@@ -0,0 +1,10 @@
+public class MathFunction {
+  public static void main(String args[])
+  {
+    System.out.println(Math.abs(-1025));
+    System.out.println(Math.ceil(52.63));
+    System.out.println(Math.ceil(52.63));
+    System.out.println(Math.round(2.63));
+    System.out.println(Math.random());
+  }
+}
