4c297d7f3f0bc1c70062f4f16d4a38a5e8400a0d
yash tomar <tomary880@gmail.com>
2026-07-19 03:16:28 +0530

    Added the file for concatenating two strings

---
+++ Java/Practise Exercise/ConcatenateTwoStrings.java
@@ -0,0 +1,9 @@
+public class ConcatenateTwoStrings {
+  public static void main(String args[])
+  {
+    String first = "Yash";
+    String sur = "Tomar";
+    String full = first + " " + sur;
+    System.out.println(full.toUpperCase());
+  }
+}
