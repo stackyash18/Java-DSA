4dc212d01f56e355772a6514a4a6ec23199451dc
yash tomar <tomary880@gmail.com>
2026-07-19 07:05:46 +0530

    Added the file concatenating array of words

--- Java/Practise Exercise/ConcatenateArrayOfWords.java
+++ Java/Practise Exercise/ConcatenateArrayOfWords.java
@@ -1,11 +1,11 @@
 public class ConcatenateArrayOfWords {
   public static void main(String args[])
   {
-    String arr[] = {"Yash"," Yashika", " Deepanshu", " Deepanshi", " Mahima"};
+    String arr[] = {"Yash"," Yashika", " Mayank", " Deepanshi", " Mahima"};
 
-    StringBuilder sb = new StringBuilder(arr[0]);
+    StringBuilder sb = new StringBuilder();
 
-    for(int i = 1; i<arr.length; i++)
+    for(int i = 0; i<arr.length; i++)
     {
      sb.append(arr[i]);
     }
